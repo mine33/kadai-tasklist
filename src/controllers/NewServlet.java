878100cd74a2cec644b2b940d3eb2c19f1a42acb
mine33 <mine.30G@gmail.com>
2021-07-05 22:12:08 +0900

    Add getAllTasks

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,7 +1,7 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
+import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -13,18 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import models.Task;
 import utils.DBUtil;
 
-
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class NewServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public NewServlet() {
         super();
     }
 
@@ -34,10 +33,26 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EntityManager em = DBUtil.createEntityManager();
 
-		List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-		response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+		// Taskのインスタンスを生成
+		Task m = new Task();
 
+		// tの各フィールドにデータを代入
+		String content = "hello";
+		m.setContent(content);
 
+		Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+		m.setCreated_at(currentTime);
+		m.setUpdated_at(currentTime);
+
+		// データベースに保存
+		em.getTransaction().begin();
+		em.persist(m);
+		em.getTransaction().commit();
+
+		// 自動採番されたIDの値を表示
+		response.getWriter().append(Integer.valueOf(m.getId()).toString());
+
+		em.close();
 	}
 
 }
